d6146977db5585c2756df30ca6324e149fa08cc8
Angel Donchev Angelov <ang.don.ang@gmail.com>
2024-04-11 16:23:21 +0200

    RT Support

--- VulkanTutorial/Source/VulkanTutorial.cpp
+++ VulkanTutorial/Source/VulkanTutorial.cpp
@@ -35,7 +35,19 @@ const std::vector<const char*> validationLayers = {
 };
 
 const std::vector<const char*> deviceExtensions = {
-    VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+
+    // Dependencies for Hardware RT Support:
+    VK_EXT_DESCRIPTOR_INDEXING_EXTENSION_NAME,
+    VK_KHR_DEFERRED_HOST_OPERATIONS_EXTENSION_NAME,
+    VK_KHR_SHADER_FLOAT_CONTROLS_EXTENSION_NAME,
+    VK_KHR_SPIRV_1_4_EXTENSION_NAME,
+    VK_KHR_BUFFER_DEVICE_ADDRESS_EXTENSION_NAME,
+
+    // Hardware RT Extensions
+    VK_KHR_ACCELERATION_STRUCTURE_EXTENSION_NAME,
+    VK_KHR_RAY_TRACING_PIPELINE_EXTENSION_NAME,
+    VK_KHR_RAY_QUERY_EXTENSION_NAME,
 };
 
 #ifdef NDEBUG
@@ -92,6 +104,7 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
     VkDebugUtilsMessageTypeFlagsEXT messageType,
     const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
     void* pUserData) {
+
     
     // Diagnostic Message
     if (messageSeverity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT) {
@@ -283,6 +296,11 @@ private:
         vkEnumeratePhysicalDevices(m_VKInstance, &deviceCount, devices.data());
 
         for (const auto& device : devices) {
+            VkPhysicalDeviceProperties deviceProperties;
+            vkGetPhysicalDeviceProperties(device, &deviceProperties);
+
+            VK_LOG_INFO("%s", deviceProperties.deviceName);
+
             if (isDeviceSuitable(device)) {
                 m_PhysicalDevice = device;
                 break;

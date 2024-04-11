39e9a34cff479472394db7ff96cf23e7dc129990
Angel Donchev Angelov <ang.don.ang@gmail.com>
2024-04-11 22:25:42 +0200

    .spv to binary read

--- VulkanTutorial/Source/VulkanTutorial.cpp
+++ VulkanTutorial/Source/VulkanTutorial.cpp
@@ -24,6 +24,7 @@
 #include <cstdint> // Necessary for uint32_t
 #include <limits> // Necessary for std::numeric_limits
 #include <algorithm> // Necessary for std::clamp
+#include <fstream>
 #include <iostream>
 
 constexpr uint32_t WIDTH = 800;
@@ -57,6 +58,23 @@ const bool enableValidationLayers = true;
 #endif
 
 
+static std::vector<char> readFile(const std::string& filename) {
+	std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open()) {
+        throw std::runtime_error("failed to open file!");
+    }
+
+	const size_t fileSize = file.tellg();
+    std::vector<char> buffer(fileSize);
+
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+    file.close();
+
+    return buffer;
+
+}
 
 bool checkValidationLayerSupport() {
     uint32_t layerCount;
@@ -597,6 +615,13 @@ private:
 
     }
 
+    void createGraphicsPipeline()
+    {
+        auto vertShaderCode = readFile("Shaders/Compiled/vert.spv");
+        auto fragShaderCode = readFile("Shaders/Compiled/frag.spv");
+
+    }
+
 	void InitVulkan() {
         createInstance();
         setupDebugMessenger();
@@ -605,6 +630,7 @@ private:
         createLogicalDevice();
         createSwapChain();
         createImageViews();
+        createGraphicsPipeline();
     }
 
     void MainLoop() {

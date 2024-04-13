d34552406e0c344c955b303d6b9bb3dff654988a
Angel Donchev Angelov <ang.don.ang@gmail.com>
2024-04-13 20:15:57 +0200

    Shader compile as build step

--- VulkanTutorial/Shaders/compile.bat
+++ VulkanTutorial/Shaders/compile.bat
@@ -1,4 +1,5 @@
-C:/VulkanSDK/1.3.280.0/Bin/glslc.exe shader.vert -o Compiled/vert.spv
-C:/VulkanSDK/1.3.280.0/Bin/glslc.exe shader.frag -o Compiled/frag.spv
+echo Current directory is %cd%
+C:/VulkanSDK/1.3.280.0/Bin/glslc.exe Shaders/shader.vert -o Shaders/Compiled/vert.spv
+C:/VulkanSDK/1.3.280.0/Bin/glslc.exe Shaders/shader.frag -o Shaders/Compiled/frag.spv
 pause
 
